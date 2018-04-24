bcfbecb4c5adf7db709d803a8c238bfc54fe21be
Michael V. DePalatis <mike@depalatis.net>
2018-04-24 15:18:22 -0400

    Define events dtype and intervals

--- artdet.py
+++ artdet.py
@@ -4,23 +4,61 @@ import random
 
 import numpy as np
 import pandas as pd
-from traits.api import HasTraits, Array,ListStr
+from traits.api import HasTraits, Array, CArray, ListStr
+
+# dtype for events recarrays
+_events_dtype = [
+    ('channel', '|U32'),
+    ('frequency', np.int),  # mHz
+    ('amplitude', np.int),  # uA
+    ('duration', np.int),  # ms
+    ('onset', np.int),  # ms
+    ('hashtag', '|S40'),  # sha1 hash
+]
 
 
 class ArtifactDetector(HasTraits):
     """Class used to detect artifactual and saturated channels."""
     timeseries = Array(desc='time series data, shape = (n_channels, n_samples)')
     channels = ListStr(desc='channel labels')
-    stim_events = Array(desc='stimulation events')
-    sham_events = Array(desc='sham stimulation events')
+    stim_events = Array(desc='stimulation events', dtype=_events_dtype)
+    sham_events = Array(desc='sham stimulation events', dtype=_events_dtype)
+    pre_stim_interval = CArray(value=[-440, -40], dtype=np.int, shape=(2,),
+                               desc='pre-stim start and stop times')
+    post_stim_interval = CArray(value=[40, 440], dtype=np.int, shape=(2,),
+                                desc='post-stim start and stop times')
 
-    def __init__(self, timeseries, channels, stim_events, sham_events):
+    def __init__(self, timeseries, channels, stim_events, sham_events,
+                 pre_stim_interval=None, post_stim_interval=None):
         super(ArtifactDetector, self).__init__()
 
         self.timeseries = timeseries
         self.channels = channels
         self.stim_events = stim_events
         self.sham_events = sham_events
+
+        if pre_stim_interval is not None:
+            self.pre_stim_interval = pre_stim_interval
+
+        if post_stim_interval is not None:
+            self.post_stim_interval = post_stim_interval
+
+    def _extract_intervals(self, sham=False):
+        """Extract pre- and post-stim/sham intervals.
+
+        Parameters
+        ----------
+        sham : bool
+            Whether to extract sham (True) or stim (False) intervals.
+
+        Returns
+        -------
+        dict
+            A dict containing pre and post intervals which are arrays with
+            dimensions (n_events, n_channels, n_samples_in_interval)
+
+        """
+        events = self.sham_events if sham else self.stim_events
 
     def _get_saturated_channels(self):
         """Identify channels which display post-stim saturation.
@@ -78,11 +116,27 @@ class ArtifactDetector(HasTraits):
 
 
 if __name__ == "__main__":
+    from hashlib import sha1
+
+    def hashit(x):
+        return sha1(str(x).encode())
+
     channels = ['LA1', 'LA2', 'LAD1', 'LAD2']
     samples = 10000
     ts = np.random.random((len(channels), samples))
-    stim = np.array([1, 2, 3])
-    sham = np.array([1, 2, 3])
+
+    freq = int(200 * 1e3)
+    ampl = 500
+    duration = 500
+
+    stim, sham = [], []
+
+    for i, channel in enumerate(channels):
+        stim.append((channel, freq, ampl, duration, i * 2000, hashit(i)))
+        sham.append((channel, freq, ampl, duration, (i + len(channels)) * 2000, hashit(i + len(channels))))
+
+    stim = np.rec.array(stim, dtype=_events_dtype)
+    sham = np.rec.array(sham, dtype=_events_dtype)
 
     detector = ArtifactDetector(ts, channels, stim, sham)
     print(detector.get_bad_channels())
